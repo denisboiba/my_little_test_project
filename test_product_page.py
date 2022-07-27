b80d36c281d010a99365d9762c637c37d10ca8a0
denisboiba <denisboiba97@gmail.com>
2022-07-27 17:51:54 +0200

    use parameterization including xfail mark

--- test_product_page.py
+++ test_product_page.py
@@ -3,8 +3,12 @@ from selenium import webdriver
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('num',
+                         [*range(7),
+                          pytest.param('7', marks=pytest.mark.xfail(reason='bugged page')),
+                          *range(8, 10)])
+def test_guest_can_add_product_to_basket(browser, num):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_basket_button()
